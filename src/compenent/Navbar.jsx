d2bcaaf181d2d84034659ef0b97420e5b4513d3a
Alaeddine c9 <alaeddinechraiti@gmail.com>
2024-05-31 12:03:08 +0200

    STL

--- src/compenent/Navbar.jsx
+++ src/compenent/Navbar.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import Logo from '../../public/Logo.png'
+import React from 'react';
+import logo from '../../public/Logo.png';
+
 export default function Navbar() {
     return (
         <>
-            <div className="lg:flex lg:items-center lg:justify-between">
-                <div className="min-w-0 flex-1">
-                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"><img src={Logo} alt="" width="300px" /></h2>
-                </div>
-                <div className="mt-5 flex lg:ml-4 lg:mt-0">
+            <div className="flex items-center justify-between px-5">
+                <h2>
+                    <img src={ logo } alt="Logo Cinemax" width="250px" />
+                </h2>
+                <div className="flex items-center gap-5 mt-7">
                     <a href="#">Search</a>
-                    <a href="#" className="ml-3 sm:block">Favotites</a>
-
-                    <a href="#" className="inline-flex items-center rounded-full bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Login </a>
+                    <a href="#" className="">Favorites</a>
+                    <a href="#" className="px-4 py-2 text-white bg-orange-500 rounded-full">Login</a>
                 </div>
             </div>
-
         </>
-    )
+    );
 }
